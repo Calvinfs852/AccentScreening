57a7da249654c56a10962bf49b3717f62a159ed3
Calvinfs852 <calvinfs852@gmail.com>
2015-11-07 14:24:40 -0500

    Query added
    Page now successfully adds row to local db

--- src/screening.php
+++ src/screening.php
@@ -5,6 +5,23 @@ Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
 
 
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+
+$db = mysqli_connect("localhost", "screeningapp", "8xjhuyYXCdqV", "c2230a01test", "3306");
+$query = "INSERT INTO screenings (name, phone, email) VALUES (" . $name . ", " . $phone . ", '" . $email . "');";//Single quotes around email to avoid @ error
+if (mysqli_query($db, $query)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $query . "<br>" . mysqli_error($db);
+}
+?>
+
+
+
+
 
 
 </body>
