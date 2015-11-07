e65fa62da194ffa153a1935b4265ad1ea0d0700c
Calvinfs852 <calvinfs852@gmail.com>
2015-11-07 10:55:40 -0500

    Less and js preparations for merging register.html and screening.html

--- src/js/register.js
+++ src/js/register.js
@@ -1,15 +1,7 @@
-/**
- * Created by Calvin Scott on 10/31/2015.
- */
-function goToScreening()
-{
-
-}
-
 function recordData(window)
 {
     alert("Your information has been recorded")//TODO: Record information. .Serialize()?
-    window.location = "screening.html";//Will be irrelevant
-
+    window.location = "screening.html";//Will be
+    $("$signin-box").hide();
 
 }
