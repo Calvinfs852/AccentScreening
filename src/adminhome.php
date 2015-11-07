1ead357269668910e455869ba38ced3bedec8d51
Calvinfs852 <calvinfs852@gmail.com>
2015-11-07 16:44:38 -0500

    admin homepage phpfying

--- src/adminhome.php
+++ src/adminhome.php
@@ -1,43 +1,65 @@
 <!-- example from W3Schools Tutorial on Bootstrap -->
 
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Homepage</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-  <link rel="stylesheet/less" href="css/adminhome.less" type="text/css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.3/less.min.js"></script>
+    <title>Homepage</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet/less" href="css/adminhome.less" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.3/less.min.js"></script>
 
 </head>
 <body class="pagebackground">
 
 <div class="container">
-  <div class="row">
-    <div class="col-md-2 pull-right">
-      <label for="logoutbutton">$name</label>
-      <button class="btn-warning btn-s" id="logoutbutton" value="Log Out" type="button">
+    <?php
+    $connection = mysqli_connect("localhost", "screeningapp", "8xjhuyYXCdqV", "c2230a01test", "3306");
+    $query = "SELECT name, date FROM screenings WHERE reviewed=0";
+    $result = mysqli_query($connection, $query);
+
+    if ($result) {
+        echo '<div class="table-responsive"><table class = "table">';
+        while ($row = mysqli_fetch_array($result)) {
+            $name = $row["name"];
+            $date = $row["date"];
+            include "screening_alert.php";
+        }
+        echo '</table></div>';
+    }
+    ?>
+
+
+    <div class="row">
+        <div class="col-md-2 pull-right">
+            <label for="logoutbutton">$name</label>
+            <button class="btn-warning btn-s" id="logoutbutton" value="Log Out" type="button">
+        </div>
     </div>
-    <div class="jumbotron col-md-8">
-      <form role="form">
-        <button type="button" class="btn btn-block btn-danger" >Take Assessment</button>
-      </form>
+
+    <div class="row">
+        <div class="jumbotron table-responsive">
+            <p>Reviewed screenings:</p>
+            <table class="table">
+                <?php
+                $connection = mysqli_connect("localhost", "screeningapp", "8xjhuyYXCdqV", "c2230a01test", "3306");
+                $query = "SELECT name, date FROM screenings";
+                $result = mysqli_query($connection, $query);
+
+                if ($result) {
+                    while ($row = mysqli_fetch_array($result)) {
+                        $name = $row["name"];
+                        $date = $row["date"];
+                        include "screening_alert.php";
+                    }
+                }
+                ?>
+            </table>
+        </div>
     </div>
-  </div>
-  <div class="row">
-    <div class="jumbotron">
-    <table>
-      <tr>
-        <td>
-        No data yet
-        </td>
-      </tr>
-    </table>
-  </div>
-  </div>
 </div>
 
 </body>
