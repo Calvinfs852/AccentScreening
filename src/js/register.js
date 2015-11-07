10ef589dfcd04704def558993c7eb7987893815d
Calvinfs852 <calvinfs852@gmail.com>
2015-11-07 11:04:30 -0500

    Combine register and screening
    Copied registration box over to screening.html

--- src/js/register.js
+++ src/js/register.js
@@ -1,7 +1,7 @@
-function recordData(window)
+function recordFormData(window)
 {
     alert("Your information has been recorded")//TODO: Record information. .Serialize()?
-    window.location = "screening.html";//Will be
     $("$signin-box").hide();
+    $("#screening-box").show();
 
 }
